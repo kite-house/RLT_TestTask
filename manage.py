0fc6422411197566d7cd168262621ff56d8b78c4
kite-house <hodosok1301@gmail.com>
2024-05-22 03:16:12 +0700

    crutch tg

--- manage.py
+++ manage.py
@@ -1,2 +1,17 @@
 # Телега 
-sdasd
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+import os
+
+bot = Bot(token=os.environ.get('TELEGRAM_ACCESS_TOKEN'))
+
+dp = Dispatcher()
+print('Telegram-bot запущен!')
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Прив")
+
+if __name__ == "__main__":
+    asyncio.run(dp.start_polling(bot))
