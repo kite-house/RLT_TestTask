9b93c9cc7627ba5226535262ae51dfa08784f82d
kite-house <hodosok1301@gmail.com>
2024-05-22 05:44:13 +0700

    add tg get_dataset

--- manage.py
+++ manage.py
@@ -3,15 +3,30 @@ import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 import os
+from agrigation import Agrigation
+import tests
+import unittest
+
+unittest.TextTestRunner(verbosity=2).run(unittest.TestLoader().loadTestsFromModule(tests))
 
 bot = Bot(token=os.environ.get('TELEGRAM_ACCESS_TOKEN'))
-
 dp = Dispatcher()
-print('Telegram-bot запущен!')
+
+print('Telegram-bot: launched!')
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer("Прив")
+
+@dp.message()
+async def get_dataset(message: types.Message):
+    try:
+        request = eval(message.text)
+        if list(request.keys()) == ['dt_from', 'dt_upto', 'group_type']:
+            if list(request.values()) != ['', '', '']:
+                await message.answer(Agrigation(request).dataset())
+    except Exception:
+        await message.answer('Невалидный запос. Пример запроса: {"dt_from": "2022-09-01T00:00:00", "dt_upto": "2022-12-31T23:59:00", "group_type": "month"}')
 
 if __name__ == "__main__":
     asyncio.run(dp.start_polling(bot))
