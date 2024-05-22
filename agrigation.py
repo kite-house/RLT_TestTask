84b62ae32a86b162a4fbec089f471df8c603ea27
kite-house <hodosok1301@gmail.com>
2024-05-22 16:38:49 +0700

    fix ""

--- agrigation.py
+++ agrigation.py
@@ -8,19 +8,19 @@ class Agrigation:
         self.dt_from = datetime.strptime(request['dt_from'].replace('T', ' '), '%Y-%m-%d %H:%M:%S')
         self.dt_upto = datetime.strptime(request['dt_upto'].replace('T', ' '), '%Y-%m-%d %H:%M:%S')
         self.group_type = request['group_type']
-        self.resultData = {'dataset' : [], 'labels' : []}
+        self.resultData = {"dataset" : [], "labels" : []}
 
     def group(self):
         '''Группируем данные по назначеному типу'''
         data = sorted(self.filter(), key = lambda x: x['dt']) #  сортировка данных по вермени.
         if self.group_type == 'month':
-            return {month: list(group) for month, group in groupby(data, key=lambda x: f"{x['dt'].year}-{x['dt'].month}-1")}, '%Y-%m-01T00:00:00'
+            return {month: list(group) for month, group in groupby(data, key=lambda x: f"{x['dt'].year}-{x['dt'].month}-1")}, "%Y-%m-01T00:00:00"
             
         elif self.group_type == 'day':
-            return {day: list(group) for day, group in groupby(data, key=lambda x: f"{x['dt'].year}-{x['dt'].month}-{x['dt'].day}")}, '%Y-%m-%dT00:00:00'
+            return {day: list(group) for day, group in groupby(data, key=lambda x: f"{x['dt'].year}-{x['dt'].month}-{x['dt'].day}")}, "%Y-%m-%dT00:00:00"
         
         elif self.group_type == 'hour':
-            return {hour: list(group) for hour, group in groupby(data, key=lambda x: f"{x['dt'].year}-{x['dt'].month}-{x['dt'].day}-{x['dt'].hour}")}, '%Y-%m-%dT%H:00:00'
+            return {hour: list(group) for hour, group in groupby(data, key=lambda x: f"{x['dt'].year}-{x['dt'].month}-{x['dt'].day}-{x['dt'].hour}")}, "%Y-%m-%dT%H:00:00"
     
         else:
             raise Exception('group does not match')
@@ -70,7 +70,7 @@ class Agrigation:
             for data in sortedData[time]: # получаем данные в этой группе
                 value += data['value']
 
-            self.resultData['labels'].append(datetime.strftime(sortedData[time][0]['dt'], self.formatTime))
-            self.resultData['dataset'].append(value)
+            self.resultData["labels"].append(datetime.strftime(sortedData[time][0]['dt'], self.formatTime))
+            self.resultData["dataset"].append(value)
 
-        return str(self.resultData) # исправить ошибку с пропусками
+        return str(self.resultData).replace("'", '"')
